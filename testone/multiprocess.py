d3f900a39de22a95cb5b48af0028f0a4a6fed7ad
ruoshengyuan <ruoshengyuan@163.com>
2019-09-13 14:46:03 +0800

    eee

--- testone/multiprocess.py
+++ testone/multiprocess.py
@@ -12,9 +12,10 @@ Income_Table = [
 		IncomeLookup(80000,0.45,15160),
 		IncomeLookup(55000,0.35,7160),
 		IncomeLookup(35000,0.30,4410),
-		IncomeLookup(80000,0.45,15160),
-		IncomeLookup(80000,0.45,15160),
-		IncomeLookup(80000,0.45,15160),
-		IncomeLookup(80000,0.45,15160),
-		IncomeLookup(80000,0.45,15160),
+		IncomeLookup(25000,0.25,2660),
+		IncomeLookup(12000,0.2,1410),
+		IncomeLookup(3000,0.1,210),
+		IncomeLookup(0,0.03,0)
 ]
+
+
