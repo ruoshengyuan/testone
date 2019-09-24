301bd7231b3c5f3069d47ec5c388e16c8fc86096
ruoshengyuan <ruoshengyuan@163.com>
2019-09-24 01:32:27 +0000

    2

--- testone/jisuan-Copy1.py
+++ testone/jisuan-Copy1.py
@@ -1,7 +1,9 @@
 
 import random 
 import csv
-from docx import Document
+from docx import Document,styles
+from docx.shared import Pt,Inches
+from docx.enum.style import WD_STYLE_TYPE
 
 
 def Night():
@@ -128,6 +130,58 @@ def Chu_Jia_9():
 
 CHU_JIA_9 = Chu_Jia_9()
 
+def Jia_qian1000():
+    result2 = []
+    k = 0
+    while k < 1000 :
+        i = random.randint(100,799) 
+        m = random.randint(100,899-i)
+        n = random.randint(100,1000-m-i)
+        shizi = str(i)+'+'+str(m)+'+'+str(n)+'='
+        daan = i+m+n
+        result2.append((shizi,daan))
+        k = k+1
+        
+    k = 0
+    while k < 1000 :
+        i = random.randint(100,899) 
+        m = random.randint(100,1000-i)
+        n = random.randint(100,m+i)
+        shizi3 = str(i)+'+'+str(m)+'-'+str(n)+'='
+        daan3 = i+m-n
+        result2.append((shizi3,daan3))
+        k = k+1
+    return result2
+
+
+JIA_QIAN1000 = Jia_qian1000()
+
+def Jian_qian1000():
+    result2 = []
+    k = 0
+    while k < 1000 :
+        i = random.randint(201,1000) 
+        m = random.randint(100,i)
+        n = random.randint(100,1000+m-i)
+        shizi = str(i)+'-'+str(m)+'+'+str(n)+'='
+        daan = i-m+n
+        result2.append((shizi,daan))
+        k = k+1
+        
+    k = 0
+    while k < 1000 :
+        i = random.randint(301,1000) 
+        m = random.randint(100,i-100)
+        n = random.randint(100,i-m)
+        shizi3 = str(i)+'-'+str(m)+'-'+str(n)+'='
+        daan3 = i-m-n
+        result2.append((shizi3,daan3))
+        k = k+1
+    return result2
+
+
+JIAN_QIAN1000 = Jian_qian1000()
+
 
 if __name__ == "__main__" :
     # print('chengfajiafa',len(JIA_9),
@@ -144,7 +198,11 @@ if __name__ == "__main__" :
     chufajianfa = 200
     jiaqian = 100
     jianqian = 100
+    jiaqian1000 = 500
+    jianqian1000 = 500
     result = []
+    result2 = []
+
     xuhao = []
     i = 0
     while i < chengfajiafa :
@@ -194,7 +252,24 @@ if __name__ == "__main__" :
             xuhao.append(a)
             i = i +1
 
+    xuhao = []
+    i = 0
+    while i < jiaqian1000 :
+        a = random.randint(0,len(JIA_QIAN1000)-1)
+        if a not in xuhao : 
+            result2.append(JIA_QIAN1000[a])
+            xuhao.append(a)
+            i = i +1
+    xuhao = []
+    i = 0
+    while i < jianqian1000 :
+        a = random.randint(0,len(JIAN_QIAN1000)-1)
+        if a not in xuhao : 
+            result2.append(JIAN_QIAN1000[a])
+            xuhao.append(a)
+            i = i +1
 
+    result_new2 = []
     result_new = []
     xuhao_new = []
     i = 0
@@ -204,8 +279,17 @@ if __name__ == "__main__" :
             result_new.append(result[b][0])
             xuhao_new.append(b)
             i = i +1    
-    print(result_new)
+    # print(result_new)
 
+    xuhao_new = []
+    i = 0
+    while i < len(result2) :
+        b = random.randint(0,len(result2)-1)
+        if b not in xuhao_new : 
+            result_new2.append(result2[b][0])
+            xuhao_new.append(b)
+            i = i +1    
+    # print(result_new)
     # with open('shiti.csv','w')  as csvfile:
     #     writer = csv.writer(csvfile) 
 
@@ -215,10 +299,36 @@ if __name__ == "__main__" :
     #     #     writer.writerow(result_new[item])
     #     #     i = i + 1
     #     print("Save,done")
-    new_docx = Document()
-    new_add = 
-    for result in result_new:
-        new_docx.add_paragragh
+    doc_yy = Document()
 
+    styles = doc_yy.styles
+    some1 = styles.add_style('some1',WD_STYLE_TYPE.PARAGRAPH)
+    some1.font.size=Pt(14)
+
+    i = 0 
+    while i < len(result_new)-10:
+        doc_yy.add_paragraph(result_new[i]+' '*50+result_new2[i],style=some1)
+        doc_yy.add_paragraph(text='\r')
+        doc_yy.add_paragraph(result_new[i+1],style=some1)
+        doc_yy.add_paragraph(text='\r')
+        doc_yy.add_paragraph(result_new[i+2]+' '*50+result_new2[i+1],style=some1)
+        doc_yy.add_paragraph(text='\r')
+        doc_yy.add_paragraph(result_new[i+3],style=some1)
+        doc_yy.add_paragraph(text='\r')
+        doc_yy.add_paragraph(result_new[i+4],style=some1)
+        doc_yy.add_paragraph(text='\r')
+        doc_yy.add_paragraph(result_new[i+5]+' '*50+result_new2[i+2],style=some1)
+        doc_yy.add_paragraph(text='\r')
+        doc_yy.add_paragraph(result_new[i+6],style=some1)
+        doc_yy.add_paragraph(text='\r')
+        doc_yy.add_paragraph(result_new[i+7],style=some1)
+        doc_yy.add_paragraph(text='\r')   
+        doc_yy.add_paragraph(result_new[i+8]+' '*50+result_new2[i+3],style=some1)
+        doc_yy.add_paragraph(text='\r')
+        doc_yy.add_paragraph(result_new[i+9],style=some1)
+
+        i = i + 10
+
+    doc_yy.save('jieguo.docx')
 
 
