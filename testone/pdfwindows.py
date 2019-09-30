d4f5cc2cfca60b7aa7fb1e40dc70c6d789b21bf6
ruoshengyuan <ruoshengyuan@163.com>
2019-09-30 14:40:18 +0800

    20190930

--- testone/pdfwindows.py
+++ testone/pdfwindows.py
@@ -2,6 +2,7 @@
 import re
 import os
 import shutil
+import numpy as np
 import pandas as pd
 from pdfminer.converter import PDFPageAggregator
 from pdfminer.layout import LTTextBoxHorizontal, LAParams
@@ -57,42 +58,39 @@ def parse(oldpath,filepath):
                 return(results)
         except Exception as e:     
             print("a", str(e))
+            return False
 
 def infolist(allinfo):
-    leibie = ()
-    liaohao = ()
-    kehu = ()
-    guigeshu = ()
-    riqi = ()
-    bianxie = ()
-    info = []
-    if allinfo :
-        for i in range(len(allinfo)) :
-            if re.match('.*APPROVAL.*',allinfo[i],re.I|re.M):
+    leibie = ''
+    liaohao = ''
+    kehu = ''
+    guigeshu = ''
+    riqi = ''
+    bianxie = ''
+    for s in range(len(allinfo)) :
+        if re.match('.*APP.*',allinfo[s],re.I|re.M):
+            for i in range(len(allinfo)) :
                 if re.match('.*-.*-.*-.*-.*',allinfo[i],re.I|re.M):
-                    if i-1>=0 and i+1 < len(allinfo):
+                    if (i-1>=0) and (i+1<len(allinfo)):
                         leibie = str(allinfo[i-1]).strip()
                         liaohao = str(allinfo[i]).strip()
-                        kehu = str(allinfo[i+1]).strip()
-                    else:
-                        leibie =''
-                        liaohao =''
-                        kehu =''
+                        kehu = str(allinfo[i+1]).strip()[:20]
+                        break
+
+            for i in range(len(allinfo)) :
                 if re.match('\d\d\d\d-.*-.*',allinfo[i],re.I|re.M):
-                    if i-1>=0 and i+1 < len(allinfo):
+                    if (i-1>=0) and (i+1<len(allinfo)) :
                         guigeshu = str(allinfo[i-1]).strip()
-                        riqi = str(allinfo[i]).strip()
+                        riqi = str(allinfo[i]).strip()[:20]
                         bianxie = str(allinfo[i+1]).strip()
-                    else :
-                        guigeshu =''
-                        riqi =''
-                        bianxie =''
-                info = [leibie,liaohao,kehu,guigeshu,riqi,bianxie]
-            else :
-                info = ['','','','','','',]
-    return info
+                        break
+
+            info = [leibie,liaohao,kehu,guigeshu,riqi,bianxie]
+            return info
+
+
 def wuliao(liaohao):
-    if not liaohao:
+    if re.match('.*-.*-.*-.*-.*',liaohao,re.I|re.M):
         array = str(liaohao).split('-')
         type1 = array[0].upper()
         type2,fenzu,fz,bianma = fengzhuang(type1)
@@ -110,12 +108,13 @@ def wuliao(liaohao):
         result=['','','','','','','']
     return result 
 
-def copyrename(oldfile,newname,newpath):
-    newname1 = os.path.join(newpath,newname+'.pdf')
-    if os.path.exists(newname1):
-        print("file is exists :Cannot copy "+oldfile+" To Target directory")
+def copyrename(oldfile,newfile,newpath,oldpath):
+    newfile1 = os.path.join(newpath,newfile)
+    oldfile1 = os.path.join(oldpath,oldfile)
+    if os.path.exists(newfile1):
+        print("file is exists :Cannot copy "+oldfile+" as NEW: "+newfile+" To Target directory")
     else :
-        shutil.copyfile(oldfile,newname1)
+        shutil.copyfile(oldfile1,newfile1)
 
 
 if __name__ == "__main__":
@@ -130,28 +129,47 @@ if __name__ == "__main__":
         os.mkdir(newpath)
     for pdfway in file_list:
         linshi = []
-        all_info = parse(oldpath,pdfway)
-        new_info = infolist(all_info)
         linshi.append(pdfway)
-        linshi.extend(new_info)
-        if new_info :
-            liaohao = new_info[1]
-            newname = str(new_info[2])+' '+str(new_info[1])+' '+str(new_info[3])+' '+str(new_info[4])
-        else :
-            liaohao = ''
-            newname = ''
+        if parse(oldpath,pdfway) :
+            all_info = parse(oldpath,pdfway)
+            if infolist(all_info) is not None :
+                new_info = infolist(all_info)
 
-        wuliaoinfo = wuliao(liaohao)        
-        linshi.extend(wuliaoinfo)
-        
-        linshi.append(newname)
+                if new_info[1] is not None:
 
-        results.append(linshi)
-    for i in range(len(results)) :
-        if results[i][2] :
-            copyrename(str(results[i][0]),str(results[i][-1]),newpath)
+                    wuliaoinfo = wuliao(new_info[1])        
+
+                    new_info[1] = re.sub('[\/:*?"<>|\\n\\r]','_',new_info[1])
+                    new_info[2] = re.sub('[\/:*?"<>|\\n\\r]','_',new_info[2])
+                    new_info[3] = re.sub('[\/:*?"<>|\\n\\r]','_',new_info[3])
+                    new_info[4] = re.sub('[\/:*?"<>|\\n\\r]','_',new_info[4])
+                    newname = str(new_info[2])+' '+str(new_info[1])+' '+str(new_info[3])+' '+str(new_info[4])+'.pdf'
+                    
+                    linshi.extend(new_info)
+                    linshi.extend(wuliaoinfo)
+                    linshi.append(newname)
+
+            results.append(linshi)
+
+
     final = pd.DataFrame(results)
     final.columns=['源文件','英文类','料号','客户','规格书','日期','编写','中文类','封装','产品代号','频点','频差','温度','负载','NEW_NAME']
     final.to_excel("abcdef123.xlsx")
+
+    
+    for i in results :
+        if len(i)==15 and i[2] :
+            copyrename(str(i[0]),str(i[14]),newpath,oldpath)
+
+    m = 0
+    n = 0
+    for i in results :
+        if   len(i)==15 and i[2] :
+            m = m+1
+        else :
+            n = n+1
+    print('files: '+str(len(results))+', ok: '+str(m)+', bad: '+str(n))
+
+
     # print(len(results))
     # print(results)
