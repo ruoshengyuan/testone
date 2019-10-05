14ea2f6106edc4a2aa5c4c6e5fe94a5d2cabb5f0
ruoshengyuan <ruoshengyuan@163.com>
2019-10-05 02:42:43 +0000

    2edqwdasd

--- Code/challenge.py
+++ Code/challenge.py
@@ -3,14 +3,17 @@ from openpyxl import Workbook
 import datetime
 
 def combine():
-	wb = load_workbook('/home/shiyanlou/Code/courses.xlsx')
+	wb = load_workbook('/home/shiyanlou/testone/Code/courses.xlsx')
 	ws = wb.active #dangqianbiao
 	ws3 = wb.create_sheet("combine")
 	for sheet in wb :
 		print (sheet.title)
 	ws1 = wb["students"]
 	ws2 = wb["time"]
-	
+	for i,row in enumerate(ws1.iter_rows()):
+    	for j,cell in enumerate(row):
+        	ws3.cell(row=i+1,column=j+1,value=cell.value)
+    
 	# for row in ws3.rows :
 	# 	print(row[0].value)
 		# for cell in row :
